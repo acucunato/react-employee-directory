e86f5e89797b91ab1aac18107d2c1d5d49a87e2c
Alyssa Cucunato <acucunato18@gmail.com>
2020-04-10 17:22:54 -0400

    added sort functionality

--- employee-directory/src/components/Card.js
+++ employee-directory/src/components/Card.js
@@ -9,10 +9,10 @@ function Card({ filteredList }) {
             <img src={employee.picture.medium} alt="Emp Img" />
           </div>
           <div className="col-md-1">{employee.name.first}</div>
-          <div className="col-md-1">{employee.name.last}</div>
+          <div className="col-md-2">{employee.name.last}</div>
           <div className="col-md-2">{employee.phone}</div>
           <div className="col-md-3">{employee.email}</div>
-          <div className="col-md-2">{employee.location.city}</div>
+          <div className="col-md-1">{employee.location.city}</div>
           <div className="col-md-2">{employee.dob.date.split("T")[0]}</div>
         </div>
       ))}
--- employee-directory/src/components/Search.js
+++ employee-directory/src/components/Search.js
@@ -5,6 +5,7 @@ import API from "../utils/API";
 class Search extends Component {
   state = {
     search: "",
+    sort: "",
     employees: [],
   };
 
@@ -22,6 +23,14 @@ class Search extends Component {
     this.setState({ search: e.target.value });
   };
 
+  handleSort = () => {
+    if (this.state.sort === "desc" || this.state.sort !== "asc") {
+      this.setState({ sort: "asc" });
+    } else if (this.state.sort === "asc" || this.state.sort !== "desc") {
+      this.setState({ sort: "desc" });
+    }
+  };
+
   render() {
     let filteredList = this.state.employees.filter((employee) => {
       return (
@@ -31,13 +40,43 @@ class Search extends Component {
       );
     });
 
+    const ascend = (a, b) => {
+      const empA = a.name.last.toUpperCase();
+      const empB = b.name.last.toUpperCase();
+      let compare = 0;
+      if (empA > empB) {
+        compare = 1;
+      } else if (empA < empB) {
+        compare = -1;
+      }
+      return compare * 1;
+    };
+
+    const descend = (a, b) => {
+      const empA = a.name.last.toUpperCase();
+      const empB = b.name.last.toUpperCase();
+      let compare = 0;
+      if (empA > empB) {
+        compare = 1;
+      } else if (empA < empB) {
+        compare = -1;
+      }
+      return compare * -1;
+    };
+
+    if (this.state.sort === "asc") {
+      filteredList.sort(ascend);
+    } else if (this.state.sort === "desc") {
+      filteredList.sort(descend);
+    }
+
     return (
       <>
         <form className="form-inline search-bar">
           <input
             className="form-control mr-sm-2"
             type="search"
-            placeholder="Search Employees"
+            placeholder="Search by First Name"
             value={this.state.search}
             name="search"
             onChange={this.updateList.bind(this)}
@@ -47,12 +86,15 @@ class Search extends Component {
           <div className="row">
             <div className="col-md-1 headings">image</div>
             <div className="col-md-1 headings">first name</div>
-            <div className="col-md-1 headings">
-              last name <i className="fas fa-sort"></i>
+            <div className="col-md-2 headings">
+              last name
+              <button className="btn" onClick={this.handleSort}>
+                <i className="fas fa-sort"></i>
+              </button>
             </div>
             <div className="col-md-2 headings">phone</div>
             <div className="col-md-3 headings">email</div>
-            <div className="col-md-2 headings">location</div>
+            <div className="col-md-1 headings">location</div>
             <div className="col-md-2 headings">dob</div>
           </div>
           <div>
